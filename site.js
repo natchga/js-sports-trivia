2a0c1b63b80162780d8af99db58e223c70e47f78
natchga <pp21800@gmail.com>
2025-10-20 20:58:22 -0500

    Exam 2

--- site.js
+++ site.js
@@ -1,4 +1,3 @@
-
 // import the utility functions "decodeHtml" and "shuffle"
 import { decodeHtml, shuffle } from './utils.js' 
 
@@ -11,12 +10,47 @@ const nextQuestionElement = document.querySelector('#nextQuestion')
 ;(async () => {
 
 	// todo: create your "getNextQuestion" function
+	const getNextQuestion = async () => {
+		const url = 'https://opentdb.com/api.php?amount=1&category=21&difficulty=easy&type=multiple'
+		const json = await fetch(url).then(response => response.json())
+		const { question, correct_answer: correct, incorrect_answers: incorrect } = json.results[0]
+		const answers = shuffle([ ...incorrect, correct ])
+		return { question, answers, correct }
+	}
 
 	// todo: create your "renderQuestion" function
+	const renderQuestion = ({ question, answers, correct }) => {
+		questionElement.textContent = decodeHtml(question)
+		answersElement.innerHTML = ''
+		
+		answers.forEach(answer => {
+			const button = document.createElement('button')
+			button.textContent = decodeHtml(answer)
+			
+			button.addEventListener('click', () => {
+				if (answer === correct) {
+					button.classList.add('correct')
+					answersElement.querySelectorAll('button').forEach(b => b.disabled = true)
+					alert('Correct!')
+					return
+				}
+				
+				button.disabled = true
+				alert('Incorrect!')
+			})
+			
+			answersElement.appendChild(button)
+		})
+	}
 
 	// todo: add the event listener to the "nextQuestion" button
+	nextQuestionElement.addEventListener('click', async () => {
+		renderQuestion(await getNextQuestion())
+		nextQuestionElement.disabled = true
+		setTimeout(() => nextQuestionElement.disabled = false, 10000)
+	})
+
+	// mimic a click on the "nextQuestion" button to show the first question
+	renderQuestion(await getNextQuestion()) // This needs to be inside the IIFE to work
 
 })()
-
-// mimic a click on the "nextQuestion" button to show the first question
-nextQuestionElement.click()
